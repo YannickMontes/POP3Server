b557fea3117004345f503c70ca2a679e273c8c66
MONTES Yannick <montes.yannick@gmail.com>
2017-03-07 15:30:48 +0100

    Handle each event received.
    Lines are commented because some of the class or not created yet.

--- src/server/Manager.java
+++ src/server/Manager.java
@@ -8,6 +8,7 @@ package server;
 import events.APOPEvent;
 import states.State;
 import events.EventEnum;
+import utils.Utils;
 
 /**
  *
@@ -39,39 +40,83 @@ public class Manager
         switch(command)
         {
             case APOP:
-                System.out.println("APOP");
+                System.out.println("[DEBUG]APOP recu");
                 
                 if(message_split.length != 3)
                 {
                     //Envoyer ERR
                     System.out.println("Arguments non valides pour la commande APOP");
-                    return "-ERR Invalid arguments\n";
+                    returnedMessage = "-ERR APOP command take two arguments\n";
                 }
-                String user = message_split[1];
-                String pass = message_split[2];
+                else
+                {
+                    String user = message_split[1];
+                    String pass = message_split[2];          
+                    returnedMessage = currentState.LauchAPOP(new APOPEvent(user, pass));
+                }
                 
-                
-                returnedMessage = currentState.LauchAPOP(new APOPEvent(user, pass));
                 break;
                 
             case STAT:
-                System.out.println("STAT");
+                System.out.println("[DEBUG]STAT recu");
+                
+                if(message_split.length != 1)
+                {
+                    returnedMessage = "-ERR STAT command take no arguments\n";
+                }
+                else
+                {
+                    //returnedMessage = currentState.LauchSTAT(new STATEvent());
+                }
+                
                 break;
                 
             case LIST:
-                System.out.println("LIST");
+                System.out.println("[DEBUG]LIST recu");
+                
+                if(message_split.length > 2)
+                {
+                    returnedMessage = "-ERR LIST command take only one optional argument\n";
+                }
+                else if(message_split.length == 2)
+                {
+                    //returnedMessage = currentState.LauchLIST(new LISTEvent(message_split[1]));
+                }
+                else
+                {
+                    //returnedMessage = currentState.LauchLIST(new LISTEvent());
+                }
+                
                 break;
                 
             case DELE:
-                System.out.println("DELE");
+                System.out.println("[DEBUG]DELE recu");
+                
+                if(message_split.length != 2)
+                {
+                    returnedMessage = "-ERR DELE command take one argument\n";
+                }
+                else
+                {
+                    //returnedMessage = currentState.LaunchDELE(new DELEEvent(message_split[1]));
+                }
                 break;
                 
             case QUIT:
-                System.out.println("QUIT");
+                System.out.println("[DEBUG]QUIT recu");
+                if(message_split.length != 1)
+                {
+                    returnedMessage = "-ERR QUIT Command only take no arguments\n";
+                }
+                else
+                {
+                    //returnedMessage = currentState.LaunchQUIT(new QUITEvent());
+                }
                 break;
             
             default:
                 System.out.println("Switch case not handled yet");
+                returnedMessage = Utils.GenerateHelpMessage();
                 break;
         }
         
