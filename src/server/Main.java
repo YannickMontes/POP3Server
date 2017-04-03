95a1784a5a172fc91ffe659c997418bc7a3b4c11
MONTES Yannick <montes.yannick@gmail.com>
2017-04-03 16:03:14 +0200

    Yoann pas bon
    Il avait pas initialiser la fênetre avant

--- src/server/Main.java
+++ src/server/Main.java
@@ -13,12 +13,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+                
+        ServerGUI frame = new ServerGUI();
+        frame.setVisible(true);
         ParserJSON.initJSONFiles();
         POP3Server pop3server = new POP3Server();
         pop3server.initializeServer();
-        
-        ServerGUI frame = new ServerGUI();
-        frame.setVisible(true);
+
     }
     
 }
